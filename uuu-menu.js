delcypher/uuu-website-mockup2013

commit e9cd7edb8a093b0524af62b60ab0ee3458caacc9

Replaced getting text from node method with elegant (code wise) with inefficient but effective method because firstChild.textContent property doesn't work in IE8.

uuu-menu.js
@@ -12,7 +12,8 @@ function uuuMenuSetBreadcrumbs(topLevel,botLevel)
 
   $("#top_level_nav").children('li').each(function(index)
   {
-    var text=$(this).get(0).firstChild.textContent;
+    //Overly complex way to get text (without child text)
+    var text=$(this).clone().children().remove().end().text();
 
     //Try to do regex match. We assume we don't need to escape text
     if( text.match(new RegExp("^\s*" + RegExp.escape(topLevel))) != null)
@@ -24,7 +25,8 @@ function uuuMenuSetBreadcrumbs(topLevel,botLevel)
       {
         $(this).find('li').each(function(index)
         {
-          var text=$(this).get(0).firstChild.textContent;
+          //Overly complex way to get text (without child text)
+          var text=$(this).clone().children().remove().end().text();
 
           //Try to do regex match. We assume we don't need to escape text
           if(text.match(new RegExp("^\s*" + RegExp.escape(botLevel))) != null)

commit cc035f6aa85327f94d828c6061d9eaa560e06fe7

Fixed function uuuMenuSetBreadcrumbs() broken by commit e9cd7edb8a093b0524af62b60ab0ee3458caacc9

uuu-menu.js
@@ -10,28 +10,30 @@ function uuuMenuSetBreadcrumbs(topLevel,botLevel)
   if(typeof botLevel != "string" && botLevel != null)
     console.error("Second argument invalid");
 
+  var foundTop=false,foundBot=false;
+
   $("#top_level_nav").children('li').each(function(index)
   {
-    //Overly complex way to get text (without child text)
-    var text=$(this).clone().children().remove().end().text();
+    var text=$(this).text();
 
     //Try to do regex match. We assume we don't need to escape text
     if( text.match(new RegExp("^\s*" + RegExp.escape(topLevel))) != null)
     {
       //found top level match
       uuuMenuTopLevelSelect.call($(this),false);
+      foundTop=true;
 
       if(botLevel != null)
       {
         $(this).find('li').each(function(index)
         {
-          //Overly complex way to get text (without child text)
-          var text=$(this).clone().children().remove().end().text();
+          var text=$(this).text();
 
           //Try to do regex match. We assume we don't need to escape text
           if(text.match(new RegExp("^\s*" + RegExp.escape(botLevel))) != null)
           {
             uuuMenuBotLevelSelect.call($(this),false);
+            foundBot=true;
             return false; //Stop loop
           }
         });
@@ -40,6 +42,9 @@ function uuuMenuSetBreadcrumbs(topLevel,botLevel)
       return false; //Stop loop
     }
   });
+
+  if(!foundTop) console.error("Could not find top level menu element:" + topLevel);
+  if(!foundBot && botLevel != null) console.error("Could not find menu element: " + topLevel + " > " + botLevel);
 }
 
 /*